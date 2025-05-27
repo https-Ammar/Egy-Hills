39bff197f588aca0140dece34d7937421c3000d3
Ammar Ahmed <ammarahmed132004@outlook.com>
2025-05-28 02:42:34 +0300

    ED

---
+++ assets/script/app.js
@@ -0,0 +1,32 @@
+let navbar = `
+    <nav class="navbar navbar-expand-lg bg-white shadow-sm py-3 mb-5">
+                    <div class="container">
+                        <a class="navbar-brand fw-bold text-dark" href="#">
+                            spaciaz
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item"><a class="nav-link" href="#">Demos</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Pages</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">News</a></li>
+                                <li class="nav-item"><a class="nav-link fw-bold" href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                        <div class="d-none d-lg-flex align-items-center">
+                            <span class="me-3 text-muted">Call Us: <span class="text-dark">+(084) 123 - 456
+                                    88</span></span>
+                            <a href="#" class="btn btn-warning fw-bold">Get In Touch</a>
+                        </div>
+                    </div>
+                </nav>
+
+`;
+
+let nav_bar = document.getElementById("nav_bar");
+nav_bar.innerHTML = navbar;
