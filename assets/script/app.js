d7c2229e3fccfab610366e8a9240cc20173c0bfe
Ammar Ahmed <ammarahmed132004@outlook.com>
2025-05-28 19:53:53 +0300

    ED

--- assets/script/app.js
+++ assets/script/app.js
@@ -2,7 +2,7 @@ let navbar = `
     <nav class="navbar navbar-expand-lg bg-white shadow-sm py-3 mb-5">
                     <div class="container">
                         <a class="navbar-brand fw-bold text-dark" href="#">
-                            spaciaz
+                    Egy-Hills
                         </a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarNav">
@@ -10,18 +10,23 @@ let navbar = `
                         </button>
                         <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
                             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                                <li class="nav-item"><a class="nav-link" href="#">Demos</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Pages</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
                                 <li class="nav-item"><a class="nav-link" href="#">Projects</a></li>
-                                <li class="nav-item"><a class="nav-link" href="#">News</a></li>
-                                <li class="nav-item"><a class="nav-link fw-bold" href="#">Contact</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Services</a></li>
+                                <li class="nav-item"><a class="nav-link" href="#">Privacy Policy</a></li>
+
+                                          <li class="nav-item"><a class="nav-link" href="#">Booking</a></li>
+
+                                <li class="nav-item"><a class="nav-link" href="#">Partners</a></li>
+
                             </ul>
                         </div>
                         <div class="d-none d-lg-flex align-items-center">
                             <span class="me-3 text-muted">Call Us: <span class="text-dark">+(084) 123 - 456
                                     88</span></span>
-                            <a href="#" class="btn btn-warning fw-bold">Get In Touch</a>
+                            <a href="#" class="btn btn-warning fw-bold">
+Contact Us</a>
                         </div>
                     </div>
                 </nav>
